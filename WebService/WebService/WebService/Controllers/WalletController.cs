6838f8471418114b258e9bcf8721f0f367c544d0
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2019-04-10 18:20:28 -0400

    Update WalletController.cs

--- WebService/WebService/WebService/Controllers/WalletController.cs
+++ WebService/WebService/WebService/Controllers/WalletController.cs
@@ -34,15 +34,34 @@ namespace WebService.Controllers
             }
         }
 
+        public string CalculateMD5Hash(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
         // POST: api/Wallet
         [HttpPost]
         public string Post(string value)
         {
             var crypt = new SHA256Managed();
+            Random random = new Random();
+            var r = random.Next(1001, 99999);
+            string salt = CalculateMD5Hash(r + "");
             string hash = String.Empty;
             var currDate = DateTime.Now;
             string timeStamp = currDate.ToString("yyyyMMddHHmmssffff");
-            byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(value + timeStamp));
+            byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(salt + timeStamp));
             foreach (byte theByte in crypto)
             {
                 hash += theByte.ToString("x2");
