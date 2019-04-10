5445d3acc41962cfdcc08d233f705441e020a71b
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2019-04-10 18:19:03 -0400

    Update WalletController.cs

--- WebService/WebService/WebService/Controllers/WalletController.cs
+++ WebService/WebService/WebService/Controllers/WalletController.cs
@@ -24,7 +24,7 @@ namespace WebService.Controllers
         {
             try
             {
-                var reader = DatabaseHelper.GetInstance().RetrieveData<Wallet>("SELECT * FROM Wallets where Hash = '" + id + "'", DbModelParser.ParseWallet,"");
+                var reader = DatabaseHelper.GetInstance().RetrieveData<Wallet>("SELECT * FROM Wallets where Hash = '" + id + "'", DbModelParser.ParseWallet, "");
 
                 return reader.FirstOrDefault();
             }
@@ -34,22 +34,41 @@ namespace WebService.Controllers
             }
         }
 
+
+        public string CalculateMD5Hash(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
         // POST: api/Wallet
         [HttpPost]
-        public string Post(string value)
+        public string Post()
         {
             var crypt = new SHA256Managed();
-
+            Random random = new Random();
+            var r = random.Next(1001, 99999);
+            string salt = CalculateMD5Hash(r + "");
             string hash = String.Empty;
             var currDate = DateTime.Now;
             string timeStamp = currDate.ToString("yyyyMMddHHmmssffff");
-            byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(value + timeStamp));
+            byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(salt + timeStamp));
             foreach (byte theByte in crypto)
             {
                 hash += theByte.ToString("x2");
             }
             InsertWallet(hash);
-
+            var json = JsonConvert.SerializeObject(new Wallet(hash, 1000));
             return hash;
         }
 
@@ -58,15 +77,6 @@ namespace WebService.Controllers
             var cmd = "INSERT INTO WALLETS VALUES(?wallet,1000)";
             cmd = cmd.Replace("?wallet", "'" + hashedWallet + "'");
             DatabaseHelper.GetInstance().ExecuteSQL(cmd);
-            var insertStatement = "INSERT INTO TRANSACTION (FromWallet,ToWallet,Content,Datetime) Values";
-            var baseInsertValues = " (?fromWallet,?toWallet,?content,?datetime)";
-            var initWalletTx = new Transaction(0, "Gift", hashedWallet, 1000);
-            var insertRow = baseInsertValues.Replace("?fromWallet", "'" + initWalletTx.FromWallet + "'");
-            insertRow = insertRow.Replace("?toWallet", "'" + initWalletTx.ToWallet + "'");
-            insertRow = insertRow.Replace("?content", initWalletTx.Content.ToString());
-            insertRow = insertRow.Replace("?content", "'" + initWalletTx.Date + "'");
-            insertStatement += insertRow;
-            DatabaseHelper.GetInstance().ExecuteSQL(insertStatement);
         }
 
     }
